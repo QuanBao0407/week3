bba4d9e69baa24612127b385d3de0707cf32572c
QuanBao0407 <114390731+QuanBao0407@users.noreply.github.com>
2023-05-08 04:59:58 +0700

    Add files via upload

---
+++ GFGP1.java
@@ -0,0 +1,13 @@
+public class GFGP1 {
+    public static void main(String[] args) {
+        // Initializing String variable with null value
+        String str = null;
+        String message = (str == null) ? "" : str.substring(0, 5);
+        System.out.println(message);
+
+        // Initializing String variable with null value
+        str = "Geeksforgeeks";
+        message = (str == null) ? "" : str.substring(0, 5);
+        System.out.println(message);
+    }
+}
