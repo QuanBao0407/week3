8264d7d8c8c46b83f79ec6c7316880cb11d6feaa
QuanBao0407 <114390731+QuanBao0407@users.noreply.github.com>
2023-05-08 05:07:25 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String args[])
+    {
+        try {
+
+            throw new MyException("GeeksGeeks");
+        }
+        catch (MyException ex) {
+            System.out.println("Caught");
+            System.out.println(ex.getMessage());
+        }
+    }
+}
